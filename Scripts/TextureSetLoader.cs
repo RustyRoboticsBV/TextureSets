36509600f6370d838ec37eec4904cf1b468537a7
RustyRoboticsBV <rusty.robotics.bv@gmail.com>
2026-04-19 19:17:34 +0200

    Working on ini support.

--- Scripts/TextureSetLoader.cs
+++ Scripts/TextureSetLoader.cs
@@ -37,11 +37,27 @@ namespace Rusty.Textures
                         stream.CopyTo(reader);
                         byte[] bytes = reader.ToArray();
 
-                        if (entry.Name.EndsWith(".png"))
+                        string lowercase = entry.Name.ToLower();
+                        string extension = lowercase.Substring(lowercase.LastIndexOf('.') + 1);
+                        string name = entry.Name.Substring(0, entry.Name.Length - extension.Length);
+                        if (extension == "bmp")
                         {
-                            string name = entry.Name.Substring(0, entry.Name.Length - 4);
+                            names.Add(name);
+                            textures.Add(CreateBmpTexture(name, bytes));
+                        }
+                        else if (extension == "png")
+                        {
                             names.Add(name);
                             textures.Add(CreatePngTexture(name, bytes));
+                        }
+                        else if (extension == "jpg" || extension == "jpeg")
+                        {
+                            names.Add(name);
+                            textures.Add(CreateJpgTexture(name, bytes));
+                        }
+                        else if (extension == "ini")
+                        {
+
                         }
                     }
                 }
@@ -57,24 +73,69 @@ namespace Rusty.Textures
         }
 
         /* Private methods. */
+        private static Texture2D CreateBmpTexture(string name, byte[] bytes)
+        {
+#if GODOT
+            Image image = new Image();
+            image.LoadBmpFromBuffer(bytes);
+            return MakeTexture(image);
+#elif UNITY_5_3_OR_NEWER
+            return LoadUnityTexture(name, bytes);
+#else
+            throw new NotImplementedException($"{nameof(TextureSetLoader)}.{nameof(CreatePngTexture)} cannot be used in this context.");
+#endif
+        }
+
         private static Texture2D CreatePngTexture(string name, byte[] bytes)
         {
 #if GODOT
             Image image = new Image();
             image.LoadPngFromBuffer(bytes);
-
-            ImageTexture texture = new ImageTexture();
-            texture.SetImage(image);
-            texture.ResourceName = name;
-            return texture;
+            return MakeTexture(image);
 #elif UNITY_5_3_OR_NEWER
-            Texture2D texture = new Texture2D(1, 1);
-            texture.LoadImage(bytes);
-            texture.name = name;
-            return texture;
+            return LoadUnityTexture(name, bytes);
 #else
             throw new NotImplementedException($"{nameof(TextureSetLoader)}.{nameof(CreatePngTexture)} cannot be used in this context.");
 #endif
         }
+
+        private static Texture2D CreateJpgTexture(string name, byte[] bytes)
+        {
+#if GODOT
+            Image image = new Image();
+            image.LoadJpgFromBuffer(bytes);
+            return MakeTexture(image);
+#elif UNITY_5_3_OR_NEWER
+            return LoadUnityTexture(name, bytes);
+#else
+            throw new NotImplementedException($"{nameof(TextureSetLoader)}.{nameof(CreatePngTexture)} cannot be used in this context.");
+#endif
+        }
+
+#if UNITY_5_3_OR_NEWER
+        /// <summary>
+        /// Load a PNG, JPG or EXR texture as a Unity texture.
+        /// </summary>
+        private static Texture2D LoadUnityTexture(string name, byte[] bytes)
+        {
+            Texture2D texture = new Texture2D(1, 1);
+            texture.LoadImage(bytes);
+            texture.name = name;
+            return texture;
+        }
+#endif
+
+#if GODOT
+        /// <summary>
+        /// Create a Godot texture from a Godot image.
+        /// </summary>
+        private static Texture2D MakeTexture(Image image)
+        {
+            ImageTexture texture = new ImageTexture();
+            texture.SetImage(image);
+            texture.ResourceName = name;
+            return texture;
+        }
+#endif
     }
 }
